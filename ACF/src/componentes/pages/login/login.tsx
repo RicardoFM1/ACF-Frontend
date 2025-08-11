dc4de7cbcf93c42197bdc8caed08b477a670ad8c
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-08-11 20:16:19 -0300

    ola

--- ACF/src/componentes/pages/login/login.tsx
+++ ACF/src/componentes/pages/login/login.tsx
@@ -6,5 +6,12 @@ export const Login=()=>{
     <h1>ACF</h1>
     <div className={style.btnCadastro}>Cadastro</div>
    </header>
+   <div className={style.divFundo}>
+    <div className={style.Login}>
+        <h2>login</h2>
+        <p>email</p>
+        <input type="text" placeholder="digi"/>
+    </div>
+   </div>
     </>
 }
