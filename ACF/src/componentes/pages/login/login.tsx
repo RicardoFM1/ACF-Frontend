3d6f93c1e225cea372e04e8555043cf09a8b3abd
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-08-11 20:30:00 -0300

    ola

--- ACF/src/componentes/pages/login/login.tsx
+++ ACF/src/componentes/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import style from "./login.module.css"
 
-export const Login=()=>{
+export const Login=()=>{ 
+
   return  <>
    <header className={style.header}>
     <h1>ACF</h1>
@@ -9,8 +10,19 @@ export const Login=()=>{
    <div className={style.divFundo}>
     <div className={style.Login}>
         <h2>login</h2>
+        <div className={style.formulario}>
+<div className={style.inputs}>
         <p>email</p>
-        <input type="text" placeholder="digi"/>
+        <input type="text" placeholder="digite seu email..." />
+        </div>
+        <div className={style.inputs}>
+        <p>senha</p>
+        <input type="text" placeholder="digite sua senha..." />
+        </div>
+        </div>
+        
+        <button className={style.butnLogin}>Fazer login</button>
+
     </div>
    </div>
     </>
