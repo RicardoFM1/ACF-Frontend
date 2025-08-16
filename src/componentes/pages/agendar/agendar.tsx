0aed8ceadefb8b108504c3a9af5b3b51f482436a
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-08-15 21:52:30 -0300

    incompleto agenda

--- src/componentes/pages/agendar/agendar.tsx
+++ src/componentes/pages/agendar/agendar.tsx
@@ -9,11 +9,26 @@ export const Agendar=()=>{
         </div>
             <h1 className={style.h1Agendar}>Agendamentos</h1>
     </header>
-    <h2>Agende seu campo de futebol</h2>
+    <h2 className={style.h2Agendamento}>Agende seu campo de futebol</h2>
 
-    <div className={style.divEscolhaDoCampo}>
-        <h3>escolha o campo disponivel</h3>
+    <div className={style.divPrincipalAgendamento}>
+        <div className={style.divOne}>
+        <div className={style.div1}>
+      <h3>Escolha o campo disponivel</h3>
         <button className={style.btnEscolhaDoCampo}>Escolha um campo</button>
+        </div>
+        <div className={style.div2}>
+            <h3>Escolha o horario disponivel</h3>
+            <select className={style.selectInput} name="" id=""><option value="">Horario</option></select>
+        </div>
+        <div className={style.div3}>
+            <h3>Ver meus Agendamentos</h3>
+            <button className={style.btnVizualizar}>Visualizar</button>
+        </div>
+</div>
+       
+            <input type="date" className={style.inputAgendar}/>
+    
     </div>
     </>
 }
